05d3bdfe75bd89f1a024cb24d4e1c6b5acf0a15c
jahidhasan6484 <jahidhasan6484@gmail.com>
2023-05-09 15:37:25 +0600

    server templete add

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@add.cyoia.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        //   await client.connect();
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Student Council server");
+});
+
+app.listen(port, () => {
+    console.log("Student Council server is running...");
+});
+
+  //npm run start-dev
