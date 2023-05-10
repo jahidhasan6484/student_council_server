71b013a308c62ab909352f70574627887e757434
jahidhasan6484 <jahidhasan6484@gmail.com>
2023-05-10 12:15:45 +0600

    contact form is submitted

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@add.cyoia.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@teamproject.1ksrmxp.mongodb.net/`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-        //   await client.connect();
+        await client.connect();
+        const database = client.db("student_council");
+        const contactCollection = database.collection("contact");
+
+        app.post("/contact/applyNow", async (req, res) => {
+            try {
+                await contactCollection.insertOne(req.body)
+                res.send({
+                    status: "success",
+                    message: "We will communicate with you very soon"
+                })
+            } catch {
+                res.send({
+                    status: "fail",
+                    message: "Something went wrong"
+                })
+            }
+        })
 
     }
     finally {
