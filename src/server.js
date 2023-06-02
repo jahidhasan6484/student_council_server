302890238e690a23a8b15d93243463eef3017aec
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-06-02 17:22:36 +0600

    test

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 
 const port = 5000;
 
 // Databse connection
 async function main() {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/student_council');
-        console.log("Database connection successful")
+  try {
+    await mongoose.connect(
+      "mongodb+srv://student_council:mZGlrSbaoQC3JKB2@cluster0.9e0lfzn.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Database connection successful");
 
-        app.listen(port, () => {
-            console.log(`Student Council listening on port ${port}`)
-        })
-    } catch {
-        console.log("Failed to connect with database")
-    }
+    app.listen(port, () => {
+      console.log(`Student Council listening on port ${port}`);
+    });
+  } catch {
+    console.log("Failed to connect with database");
+  }
 }
 
-main()
-
-
+main();
