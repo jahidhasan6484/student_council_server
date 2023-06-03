2490330a6702c5baa92694382cedfdd5870b6c4d
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-06-03 21:12:25 +0600

    university model update

--- src/app/modules/university/university.model.js
+++ src/app/modules/university/university.model.js
@@ -1,41 +1,52 @@
 const { default: mongoose } = require("mongoose");
 
-const universitySchema = mongoose.Schema({
+const universitySchema = mongoose.Schema(
+  {
     countryName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     universities: [
-        {
-            name: {
-                type: String,
-                require: true
-            },
-            address: {
-                type: String,
-                required: true,
-            },
-            ranking: {
-                type: Number,
-                required: true,
-            },
-            officialWebsite: {
-                type: String,
-                required: true,
-            },
-            imageURL: {
-                type: String,
-                required: true
-            },
-        }
+      {
+        name: {
+          type: String,
+          require: true,
+        },
+        address: {
+          type: String,
+          required: true,
+        },
+        ranking: {
+          type: Number,
+          required: true,
+        },
+        officialWebsite: {
+          type: String,
+          required: true,
+        },
+        imageURL: {
+          type: String,
+          required: true,
+        },
+      },
     ],
-},
-    {
-        timestamps: true,
-    }
+    documents: [
+      {
+        question: {
+          type: String,
+          require: true,
+        },
+        answer: {
+          type: String,
+          require: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const University = mongoose.model("university", universitySchema);
 module.exports = University;
-
-
