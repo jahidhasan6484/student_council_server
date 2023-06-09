3a4142ffca5c215055e70ab3d5c7db95c1c22dd4
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-06-10 01:56:52 +0600

    scholarship

--- src/app/modules/blogAndNews/blogAndNews.service.js
+++ src/app/modules/blogAndNews/blogAndNews.service.js
@@ -1,35 +1,40 @@
 const BlogAndNews = require("./blogAndNews.model");
 
 const createPostToDB = async (_data) => {
-    await BlogAndNews.create(_data)
+  await BlogAndNews.create(_data);
 };
 
 const getBlogAndNewsFromDB = async () => {
-    return await BlogAndNews.find({ }).exec();
-}
+  return await BlogAndNews.find({}).exec();
+};
+
+const getBlogAndNewsByIdFromDB = async (_id) => {
+  return await BlogAndNews.findOne({ _id: _id });
+};
 
 const getBlogAndNewsByTypeFromDB = async (_type) => {
-    return await BlogAndNews.find({ type: _type }).exec();
-}
+  return await BlogAndNews.find({ type: _type }).exec();
+};
 
 const updatePostToDB = async (_id, _newData) => {
-    await BlogAndNews.findByIdAndUpdate(_id, _newData, {
-        new: true,
-        runValidators: true,
-    });
+  await BlogAndNews.findByIdAndUpdate(_id, _newData, {
+    new: true,
+    runValidators: true,
+  });
 
-    return await BlogAndNews.find({});
-}
+  return await BlogAndNews.find({});
+};
 
 const deletePostByIdFromDB = async (_id) => {
-    await BlogAndNews.findByIdAndDelete(_id);
-    return await BlogAndNews.find({});
-}
+  await BlogAndNews.findByIdAndDelete(_id);
+  return await BlogAndNews.find({});
+};
 
 module.exports = {
-    createPostToDB,
-    getBlogAndNewsFromDB,
-    getBlogAndNewsByTypeFromDB,
-    updatePostToDB,
-    deletePostByIdFromDB
+  createPostToDB,
+  getBlogAndNewsFromDB,
+  getBlogAndNewsByTypeFromDB,
+  updatePostToDB,
+  deletePostByIdFromDB,
+  getBlogAndNewsByIdFromDB,
 };
